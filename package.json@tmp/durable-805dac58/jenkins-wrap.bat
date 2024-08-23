f57dd0ec9af3a9767776ca3dfbe0233c00368612
sanchit0612 <sanchits@lambdatest.com>
2024-08-23 13:20:18 +0530

    Add files via upload

---
+++ package.json@tmp/durable-805dac58/jenkins-wrap.bat
@@ -0,0 +1,3 @@
+@echo off 
+cmd /c call "C:\ProgramData\Jenkins\.jenkins\workspace\Miniproject\package.json@tmp\durable-805dac58\jenkins-main.bat" > "C:\ProgramData\Jenkins\.jenkins\workspace\Miniproject\package.json@tmp\durable-805dac58\jenkins-log.txt" 2>&1
+echo %ERRORLEVEL% > "C:\ProgramData\Jenkins\.jenkins\workspace\Miniproject\package.json@tmp\durable-805dac58\jenkins-result.txt"
